47be2333954cebb4f81b3d208509650723d3f452
Max Mezalon <jmezalon@gmail.com>
2021-12-19 18:55:14 -0500

    added ruby in technologies and change first project to NotesApp

--- script.js
+++ script.js
@@ -1,92 +1,105 @@
-const js = document.getElementById('js-ul');
+const js = document.getElementById("js-ul");
 
-const jsUl = document.getElementById('javascript');
+const jsUl = document.getElementById("javascript");
 
 function displayJsUl() {
-    jsUl.style.display = 'block';
+  jsUl.style.display = "block";
 }
 
 function hideJsUl() {
-    jsUl.style.display = 'none';
+  jsUl.style.display = "none";
 }
 
-js.addEventListener('click', displayJsUl);
+js.addEventListener("click", displayJsUl);
 
-js.addEventListener('mouseleave', hideJsUl);
-
+js.addEventListener("mouseleave", hideJsUl);
 
 // --------------------------
 
-const html = document.getElementById('html-ul');
+const html = document.getElementById("html-ul");
 
-const htmlUl = document.getElementById('html');
+const htmlUl = document.getElementById("html");
 
 function displayHtmlUl() {
-    htmlUl.style.display = 'block';
+  htmlUl.style.display = "block";
 }
 
-html.addEventListener('click', displayHtmlUl);
+html.addEventListener("click", displayHtmlUl);
 
 function hideHtmlUl() {
-    htmlUl.style.display = 'none';
+  htmlUl.style.display = "none";
 }
 
-html.addEventListener('mouseleave', hideHtmlUl);
-
+html.addEventListener("mouseleave", hideHtmlUl);
 
 // -------------------------
 
-const css = document.getElementById('css-ul');
+const css = document.getElementById("css-ul");
 
-const cssUl = document.getElementById('css');
+const cssUl = document.getElementById("css");
 
 function displayCssUl() {
-    cssUl.style.display = 'block';
+  cssUl.style.display = "block";
 }
 
 function hideCssUl() {
-    cssUl.style.display = 'none';
+  cssUl.style.display = "none";
 }
 
-css.addEventListener('click', displayCssUl);
+css.addEventListener("click", displayCssUl);
 
-css.addEventListener('mouseleave', hideCssUl);
-
+css.addEventListener("mouseleave", hideCssUl);
 
 // -----------------------------
 
-const rct = document.getElementById('rct-ul');
+const rct = document.getElementById("rct-ul");
 
-const rctUl = document.getElementById('react');
+const rctUl = document.getElementById("react");
 
 function displayRctUl() {
-    rctUl.style.display = 'block';
+  rctUl.style.display = "block";
 }
 
 function hideRctUl() {
-    rctUl.style.display = 'none';
+  rctUl.style.display = "none";
 }
 
-rct.addEventListener('click', displayRctUl);
+rct.addEventListener("click", displayRctUl);
 
-rct.addEventListener('mouseleave', hideRctUl);
+rct.addEventListener("mouseleave", hideRctUl);
 
 // -------------------------------
 
+const git = document.getElementById("git-ul");
 
-const git = document.getElementById('git-ul');
-
-const gitUl = document.getElementById('git');
+const gitUl = document.getElementById("git");
 
 function displayGitUl() {
-    gitUl.style.display = 'block';
+  gitUl.style.display = "block";
 }
 
 function hideGitUl() {
-    gitUl.style.display = 'none';
+  gitUl.style.display = "none";
 }
 
-git.addEventListener('click', displayGitUl);
+git.addEventListener("click", displayGitUl);
 
-git.addEventListener('mouseleave', hideGitUl);
+git.addEventListener("mouseleave", hideGitUl);
 
+// -------------------------------
+
+const ruby = document.getElementById("ruby-ul");
+
+const rubyUl = document.getElementById("ruby");
+
+function displayRubyUl() {
+  rubyUl.style.display = "block";
+}
+
+function hideRubyUl() {
+  rubyUl.style.display = "none";
+}
+
+ruby.addEventListener("click", displayRubyUl);
+
+ruby.addEventListener("mouseleave", hideRubyUl);
